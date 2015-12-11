7f233105a1f064e4c3bcad844982c115a498a02a
seunghwan oh <seunghwan.oh5@gmail.com>
2015-12-11 11:49:50 +0900

    Add Query to path

--- index.js
+++ index.js
@@ -6,24 +6,45 @@ module.exports = {
     pathList = routes;
   },
 
-  get: function(pathName, params) {
-    var i;
-    var keys = _.keys(params);
+  get: function(pathName, params, query) {
+    var i, queries = "";
+    var paramsKeys = _.keys(params);
+    var queryKeys = _.keys(query);
+
     var path = pathList[pathName];
 
     if(params) {
       if(path) {
-        for(i = 0; i < keys.length; i++) {
-          var key = keys[i];
+        for(i = 0; i < paramsKeys.length; i++) {
+          var key = paramsKeys[i];
           path = path.replace(":" + key, params[key]);
         }
-
-        return path;
       } else {
         return null;
       }
-    } else {
-      return pathList[pathName]
     }
+
+
+
+    /* Add query to path */
+
+    for(i = 0; i < queryKeys.length; i++) {
+      var key = queryKeys[i];
+
+      if(i == 0)
+        queries += "?";
+
+      queries += key;
+      queries += "=";
+      queries += query[key];
+
+      if(i != queryKeys.length - 1) {
+        queries += "&";
+      }
+    }
+
+    path += queries;
+
+    return path;
   }
 };
